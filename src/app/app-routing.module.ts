c63bbf829d1e54f36ad27059deb0eff7f13af509
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-18 15:41:18 -0300

    feat: add lazy loading to app with client as default route'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'clients' },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((module) => module.ClientsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
