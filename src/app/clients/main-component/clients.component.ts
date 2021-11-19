46462a691e74f9c84650d3fe4f1492a0093f0301
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-19 11:52:09 -0300

    feat: add clients as a observable to use async pipe

--- src/app/clients/main-component/clients.component.ts
+++ src/app/clients/main-component/clients.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../models/client';
+import { ClientsService } from '../service/clients.service';
 
 @Component({
   selector: 'app-clients',
@@ -6,10 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent {
-  public sideForm: boolean;
+  sideForm: boolean;
+  clients$: Observable<Client[]>;
 
-  constructor() {
+  constructor(private readonly clientsService: ClientsService) {
     this.sideForm = false;
+    this.clients$ = this.clientsService.list();
   }
 
   openForm(): void {
