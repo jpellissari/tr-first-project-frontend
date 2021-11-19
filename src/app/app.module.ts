186d1ca4716540c3a37e0119769f6dac4037c96b
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-19 11:50:27 -0300

    feat: add list function to cliets service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { SidebarModule } from './sidebar/sidebar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SidebarModule],
+  imports: [BrowserModule, AppRoutingModule, SidebarModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/clients/service/clients.service.ts
+++ src/app/clients/service/clients.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ClientsModule } from '../clients.module';
+import { Client } from '../models/client';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClientsService {
+  private readonly API = 'http://localhost:3000/clients';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  list(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.API);
+  }
 }
