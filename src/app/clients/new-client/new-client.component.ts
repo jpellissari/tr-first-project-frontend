1daa09c68bb8d80864c6e2596b3ba5475b5f42e8
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-22 11:11:45 -0300

    feat: ensure client can be created

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { LoadingService } from 'src/app/shared/loading.service';
+import { ToastService } from 'src/app/shared/toast.service';
+import { ClientsService } from '../service/clients.service';
 
 @Component({
   selector: 'app-new-client',
@@ -7,13 +11,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./new-client.component.scss']
 })
 export class NewClientComponent implements OnDestroy {
-  @Output() formClosedEvent: EventEmitter<void>;
-  submitted: boolean;
+  @Output() formClosedEvent = new EventEmitter<void>();
+  @Output() clientCreated = new EventEmitter<void>();
+  submitted: boolean = false;
   form: FormGroup;
+  error$ = new Subject<boolean>();
 
-  constructor(private readonly formBuilder: FormBuilder) {
-    this.formClosedEvent = new EventEmitter<void>();
-    this.submitted = false;
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly clientsService: ClientsService,
+    private readonly toastService: ToastService,
+    private readonly loading: LoadingService
+  ) {
     this.form = this.createForm();
   }
 
@@ -27,11 +36,11 @@ export class NewClientComponent implements OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.submitted = false;
-    this.form.reset();
+    this.resetForm();
   }
 
   closeForm(): void {
+    this.resetForm();
     this.formClosedEvent.emit();
   }
 
@@ -41,8 +50,34 @@ export class NewClientComponent implements OnDestroy {
 
   createClient() {
     this.submitted = true;
+    this.loading.start();
     if (this.form.valid) {
-      console.log(this.form.value);
+      this.clientsService.create(this.form.value.name).subscribe(
+        (success) => {
+          this.handleSuccess();
+        },
+        (error) => {
+          this.handleError(error);
+        }
+      );
     }
+  }
+
+  private resetForm(): void {
+    this.loading.stop();
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  private handleSuccess(): void {
+    this.resetForm();
+    this.toastService.showSuccessMessage('Client created');
+    this.clientCreated.emit();
+  }
+
+  private handleError(error: any): void {
+    this.error$.next(true);
+    this.loading.stop();
+    this.toastService.showErrorMessage(error);
   }
 }
