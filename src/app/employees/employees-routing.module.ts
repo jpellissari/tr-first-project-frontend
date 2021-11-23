a0e9af8064a63644b82002647301773e6d8c1a9a
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-23 15:16:17 -0300

    feat: add child route to employee

--- src/app/employees/employees-routing.module.ts
+++ src/app/employees/employees-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListEmployeesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class EmployeesRoutingModule { }
+export class EmployeesRoutingModule {}
