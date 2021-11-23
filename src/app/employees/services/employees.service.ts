55b947f0545d9296a42f5d195377a04bc40a208a
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-23 16:29:44 -0300

    feat: implement employee service

--- src/app/employees/services/employees.service.ts
+++ src/app/employees/services/employees.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BaseCrudService } from 'src/app/shared/services/base-crud.service';
+import { environment } from 'src/environments/environment';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeesService {
-
-  constructor() { }
+export class EmployeesService extends BaseCrudService<Employee> {
+  constructor(http: HttpClient) {
+    super(http, `${environment.api.baseUrl}/employees`);
+  }
 }
