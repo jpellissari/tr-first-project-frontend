2b5cedd5a2da497129054b41e515765df7a833f9
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-24 13:37:12 -0300

    refactor: move employee type to enum

--- src/app/employees/models/employee.ts
+++ src/app/employees/models/employee.ts
@@ -1,12 +1,18 @@
 import { Client } from 'src/app/clients/models/client';
 import { JobPosition } from 'src/app/job-positions/models/job-position';
 
+export enum EmployeeType {
+  EMPLOYEE = 'EMPLOYEE',
+  INTERN = 'INTERN',
+  PARTNER = 'PARTNER'
+}
+
 export interface Employee {
   id: string;
   client: Client;
   jobPosition: JobPosition;
   nationalIdentity: string;
-  type: 'employee' | 'partner' | 'intern';
+  type: EmployeeType;
   name: string;
   birthdate: Date;
   active: boolean;
