5536ea4a3101a547887c3d959330d3373f1e2b3c
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-26 16:00:33 -0300

    refactor: add translation to sidebar

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -4,10 +4,17 @@ import { SidebarComponent } from './sidebar.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule, RouterModule, FontAwesomeModule, NzIconModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule,
+    NzIconModule,
+    TranslateModule
+  ],
   exports: [SidebarComponent]
 })
 export class SidebarModule {}
