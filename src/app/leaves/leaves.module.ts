dfb0f46e8df7cd81e55ffb04db5fc6f9efd77faf
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-29 10:54:34 -0300

    feat: add routing to module

--- src/app/leaves/leaves.module.ts
+++ src/app/leaves/leaves.module.ts
@@ -4,9 +4,10 @@ import { AddLeaveComponent } from './components/add-leave/add-leave.component';
 import { EditLeaveComponent } from './components/edit-leave/edit-leave.component';
 import { ListLeavesComponent } from './components/list-leaves/list-leaves.component';
 import { SharedModule } from '../shared/shared.module';
+import { LeavesRoutingModule } from './leaves-routing.module';
 
 @NgModule({
   declarations: [AddLeaveComponent, EditLeaveComponent, ListLeavesComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, LeavesRoutingModule, SharedModule]
 })
 export class LeavesModule {}
