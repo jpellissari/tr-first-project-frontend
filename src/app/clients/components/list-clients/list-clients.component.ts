9e17e23a8e01625e5c4d2764f54e7398b98e86e6
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-29 18:03:08 -0300

    refactor: create edit component to refactor duplicated code

--- src/app/clients/components/list-clients/list-clients.component.ts
+++ src/app/clients/components/list-clients/list-clients.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { EMPTY, Observable, Subject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { TranslateService } from '@ngx-translate/core';
 
 import { ToastService } from 'src/app/shared/services/toast.service';
 import { LoadingService } from 'src/app/shared/services/loading.service';
-import { SideForm } from 'src/app/shared/models/side-form';
+import { ListComponent } from 'src/app/shared/components/list-component';
 import { Client } from '../../models/client';
 import { ClientsService } from '../../service/clients.service';
 
@@ -13,71 +14,21 @@ import { ClientsService } from '../../service/clients.service';
   templateUrl: './list-clients.component.html',
   styleUrls: ['./list-clients.component.scss']
 })
-export class ListClientsComponent implements OnInit {
-  sideForm!: SideForm;
-  selectedClient!: Client;
-  clients$!: Observable<Client[]>;
-  error$ = new Subject<boolean>();
-
+export class ListClientsComponent extends ListComponent<Client> {
   constructor(
-    private readonly clientsService: ClientsService,
-    private readonly toastService: ToastService,
-    private readonly loadingService: LoadingService
-  ) {}
-
-  ngOnInit() {
-    this.fetchClients();
-    this.initSideForm();
-  }
-
-  private fetchClients() {
-    this.loadingService.start();
-
-    this.clients$ = this.clientsService.list().pipe(
-      tap(() => this.loadingService.stop()),
-      catchError(() => {
-        return this.handleError();
-      })
+    clientsService: ClientsService,
+    toastService: ToastService,
+    loadingService: LoadingService,
+    translateService: TranslateService,
+    titleService: Title
+  ) {
+    super(
+      toastService,
+      loadingService,
+      clientsService,
+      translateService,
+      titleService,
+      'clients'
     );
-  }
-
-  private initSideForm() {
-    this.sideForm = {
-      status: false,
-      type: 'create'
-    };
-  }
-
-  handleError() {
-    this.loadingService.stop();
-    this.error$.next(true);
-    this.toastService.showErrorMessage(
-      'Erro ao carregar lista de clientes. Tente mais tarde.'
-    );
-    return EMPTY;
-  }
-
-  openCreateForm(): void {
-    this.sideForm = {
-      status: true,
-      type: 'create'
-    };
-  }
-
-  openUpdateForm(client: Client): void {
-    this.selectedClient = client;
-
-    this.sideForm = {
-      status: true,
-      type: 'update'
-    };
-  }
-
-  closeSideForm() {
-    this.sideForm.status = false;
-  }
-
-  refresh() {
-    this.fetchClients();
   }
 }
---
+++ src/app/shared/components/list-component.ts
@@ -0,0 +1,87 @@
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { EMPTY, Observable, Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { SideForm } from '../models/side-form';
+import { BaseCrudService } from '../services/base-crud.service';
+import { LoadingService } from '../services/loading.service';
+import { ToastService } from '../services/toast.service';
+
+export class ListComponent<T> {
+  title!: string;
+  entityList$!: Observable<T[]>;
+  selectedEntity!: T;
+  error$ = new Subject<boolean>();
+  sideForm!: SideForm;
+
+  constructor(
+    private readonly toastService: ToastService,
+    private readonly loadingService: LoadingService,
+    private readonly entityService: BaseCrudService<T>,
+    private readonly translateService: TranslateService,
+    private readonly titleService: Title,
+    private readonly entityName: string
+  ) {
+    this.loadingService.start();
+    this.setPageTitle();
+    this.initSideForm();
+    this.fetchEntityList();
+  }
+
+  private setPageTitle(): void {
+    this.translateService
+      .get(`${this.entityName}.title`)
+      .subscribe((translation) => (this.title = translation));
+    this.titleService.setTitle(`${environment.title} - ${this.title}`);
+  }
+
+  private fetchEntityList(): void {
+    this.entityList$ = this.entityService.list().pipe(
+      tap(() => this.loadingService.stop()),
+      catchError(() => {
+        return this.handleError();
+      })
+    );
+  }
+
+  private initSideForm() {
+    this.sideForm = {
+      status: false,
+      type: 'create'
+    };
+  }
+
+  private handleError() {
+    this.loadingService.stop();
+    this.error$.next(true);
+    this.toastService.showErrorMessage(
+      `Erro ao carregar lista de ${this.title}. Tente mais tarde.`
+    );
+    return EMPTY;
+  }
+
+  openCreateForm(): void {
+    this.sideForm = {
+      status: true,
+      type: 'create'
+    };
+  }
+
+  openUpdateForm(entity: T): void {
+    this.selectedEntity = entity;
+
+    this.sideForm = {
+      status: true,
+      type: 'update'
+    };
+  }
+
+  closeSideForm() {
+    this.sideForm.status = false;
+  }
+
+  refresh(): void {
+    this.fetchEntityList();
+  }
+}
