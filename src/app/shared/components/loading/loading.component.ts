5b819d90c9815f89f67632dea665c42125265326
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-30 16:05:37 -0300

    feat: add shared loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent {
+  @Input() error$: Observable<boolean> = of(false);
+  @Input() errorMessage: string = '';
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,17 +6,19 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [LoadingComponent],
+  imports: [CommonModule, TranslateModule, NgxSpinnerModule],
   exports: [
     TranslateModule,
     ReactiveFormsModule,
     NgxSpinnerModule,
     NzTableModule,
     NzPopconfirmModule,
-    NzIconModule
+    NzIconModule,
+    LoadingComponent
   ]
 })
 export class SharedModule {}
