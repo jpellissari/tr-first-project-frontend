e5ee572de3ecf87b252220f4d45c88657bfa19c4
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-06 15:47:02 -0300

    feat: add global validation component

---
+++ src/app/shared/forms/validation/validation.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnChanges,
+  Input
+} from '@angular/core';
+import { ValidationError } from '../validation-errors';
+
+@Component({
+  selector: 'app-validation',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './validation.component.html',
+  styleUrls: ['./validation.component.scss']
+})
+export class ValidationComponent implements OnChanges {
+  @Input() validationErrors: ValidationError | null = null;
+
+  errorMessage: string | null = null;
+
+  ngOnChanges(): void {
+    this.validationErrors ? this.getErrorMessage() : (this.errorMessage = null);
+  }
+
+  getErrorMessage(): void {
+    const error = this.validationErrors;
+
+    if (error) {
+      error
+        .buildMessage()
+        .subscribe((message) => (this.errorMessage = message));
+    }
+  }
+}
