d033b915b45b253a1b8b66b6fc5d298937ade54e
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-07 10:35:16 -0300

    feat: add notifier to destroy subscriptions

--- src/app/clients/components/add-client/add-client.component.ts
+++ src/app/clients/components/add-client/add-client.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 import { ToastService } from 'src/app/shared/services/toast.service';
 import { NewClient } from '../../models/new-client';
@@ -13,16 +14,47 @@ import { ClientsService } from '../../service/clients.service';
 export class AddClientComponent implements OnDestroy {
   @Output() formClosedEvent = new EventEmitter<void>();
   @Output() clientCreated = new EventEmitter<void>();
+
   form: FormGroup;
-  error$ = new Subject<boolean>();
+
+  private subscriptionDestroyer: Subject<void> = new Subject();
 
   constructor(
     private readonly formBuilder: FormBuilder,
-    private readonly clientsService: ClientsService,
-    private readonly toastService: ToastService,
-    private readonly loadingService: LoadingService
+    private clientsService: ClientsService,
+    private loadingService: LoadingService,
+    private toastService: ToastService
   ) {
     this.form = this.createForm();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionDestroyer.next();
+    this.subscriptionDestroyer.complete();
+  }
+
+  closeForm(): void {
+    this.form.reset();
+    this.formClosedEvent.emit();
+  }
+
+  createClient() {
+    if (this.form.valid) {
+      this.loadingService.start();
+
+      const newClient = this.createRequestBody(this.form.value.name);
+      this.clientsService
+        .create(newClient)
+        .pipe(takeUntil(this.subscriptionDestroyer))
+        .subscribe(
+          (success) => {
+            this.handleSuccess();
+          },
+          (error) => {
+            this.handleError(error);
+          }
+        );
+    }
   }
 
   private createForm(): FormGroup {
@@ -34,43 +66,17 @@ export class AddClientComponent implements OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    this.resetForm();
-  }
-
-  closeForm(): void {
-    this.resetForm();
-    this.formClosedEvent.emit();
-  }
-
-  createClient() {
-    if (this.form.valid) {
-      this.loadingService.start();
-      const client = NewClient.create(this.form.value.name);
-      this.clientsService.create(client).subscribe(
-        (success) => {
-          this.handleSuccess();
-        },
-        (error) => {
-          this.handleError(error);
-        }
-      );
-    }
-  }
-
-  private resetForm(): void {
-    this.loadingService.stop();
-    this.form.reset();
+  private createRequestBody(name: string): NewClient {
+    return NewClient.create(name);
   }
 
   private handleSuccess(): void {
-    this.resetForm();
+    this.loadingService.stop();
     this.toastService.showSuccessMessage('Client created');
     this.clientCreated.emit();
   }
 
   private handleError(error: any): void {
-    this.error$.next(true);
     this.loadingService.stop();
     this.toastService.showErrorMessage(error);
   }
