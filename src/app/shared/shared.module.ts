45f08bdb05d1a64c4ac3f8072902f7897fa27cf4
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-07 11:32:58 -0300

    refactor: remove unused imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { LoadingComponent } from './components/loading/loading.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
-import { InputComponent } from './components/form/input/input.component';
-import { ValidationComponent } from './components/form/validation/validation.component';
-import { DeleteButtonComponent } from './components/form/delete-button/delete-button.component';
 import { CustomFormComponentsModule } from './components/form/custom-form-components.module';
 
 @NgModule({
