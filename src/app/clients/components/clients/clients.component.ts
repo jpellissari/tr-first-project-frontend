0b3c7ae36b39f3b05a9f8d96b0c1dc6770f13966
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-07 14:06:05 -0300

    refactor: transform sideform into class

--- src/app/clients/components/clients/clients.component.ts
+++ src/app/clients/components/clients/clients.component.ts
@@ -1,33 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 import { TranslateService } from '@ngx-translate/core';
 
 import { ToastService } from 'src/app/shared/services/toast.service';
 import { LoadingService } from 'src/app/shared/services/loading.service';
-import { ListComponent } from 'src/app/shared/components/list-component';
 import { Client } from '../../models/client';
+import { Observable, Subject, EMPTY, of } from 'rxjs';
+import { tap, catchError, takeUntil } from 'rxjs/operators';
+import { SideForm, SideFormType } from 'src/app/shared/models/side-form';
+import { environment } from 'src/environments/environment';
 import { ClientsService } from '../../service/clients.service';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html'
 })
-export class ClientsComponent extends ListComponent<Client> {
+export class ClientsComponent implements OnInit, OnDestroy {
+  title: string = '';
+  selectedEntity: Client = {} as Client;
+  sideForm: SideForm = new SideForm(false, 'create');
+  loadingErrorMessage: string = '';
+  entityName: string = '';
+
+  error$: Subject<boolean> = new Subject<boolean>();
+  entityList$: Observable<Client[]> = of();
+
+  private subscriptionDestroyer: Subject<void> = new Subject();
+
   constructor(
-    clientsService: ClientsService,
-    toastService: ToastService,
-    loadingService: LoadingService,
-    translateService: TranslateService,
-    titleService: Title
-  ) {
-    super(
-      toastService,
-      loadingService,
-      clientsService,
-      translateService,
-      titleService,
-      'clients'
+    private readonly toastService: ToastService,
+    private readonly loadingService: LoadingService,
+    private readonly clientService: ClientsService,
+    private readonly translateService: TranslateService,
+    private readonly titleService: Title
+  ) {}
+
+  ngOnInit(): void {
+    this.entityName = 'clients';
+    this.loadingService.start();
+    this.setPageTitle();
+    this.setLoadingErrorMessage();
+    this.fetchEntityList();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionDestroyer.next();
+    this.subscriptionDestroyer.complete();
+  }
+
+  openCreateForm(): void {
+    this.sideForm.type = 'create';
+    this.sideForm.open();
+  }
+
+  openUpdateForm(client: Client): void {
+    this.selectedEntity = client;
+
+    this.sideForm.type = 'update';
+    this.sideForm.open();
+  }
+
+  closeSideForm() {
+    this.sideForm.close();
+  }
+
+  handleClientCreated(): void {
+    this.refresh();
+    this.closeSideForm();
+  }
+
+  refresh(): void {
+    this.fetchEntityList();
+  }
+
+  private setPageTitle(): void {
+    this.translateService
+      .get(`${this.entityName}.title`)
+      .pipe(takeUntil(this.subscriptionDestroyer))
+      .subscribe((translation) => (this.title = translation));
+    this.titleService.setTitle(`${environment.title} - ${this.title}`);
+  }
+
+  private setLoadingErrorMessage(): void {
+    this.translateService
+      .get('loading.errors.listLoading', {
+        entity: this.title.toLowerCase()
+      })
+      .pipe(takeUntil(this.subscriptionDestroyer))
+      .subscribe((translation) => (this.loadingErrorMessage = translation));
+  }
+
+  private fetchEntityList(): void {
+    this.entityList$ = this.clientService.list().pipe(
+      tap(() => this.loadingService.stop()),
+      catchError(() => {
+        return this.handleError(this.loadingErrorMessage);
+      })
     );
+  }
+
+  private handleError(message: string) {
+    this.loadingService.stop();
+    this.error$.next(true);
+    this.toastService.showErrorMessage(message);
+    return EMPTY;
   }
 }
--- src/app/shared/models/side-form.ts
+++ src/app/shared/models/side-form.ts
@@ -1,6 +1,33 @@
-type SideForm = {
-  status: boolean;
-  type: 'create' | 'update';
-};
+type SideFormType = 'create' | 'update';
 
-export { SideForm };
+class SideForm {
+  private _status: boolean;
+  private _type: SideFormType;
+
+  constructor(status: boolean, type: SideFormType) {
+    this._status = status;
+    this._type = type;
+  }
+
+  get status(): boolean {
+    return this._status;
+  }
+
+  get type(): SideFormType {
+    return this._type;
+  }
+
+  set type(type: SideFormType) {
+    this._type = type;
+  }
+
+  open(): void {
+    this._status = true;
+  }
+
+  close(): void {
+    this._status = false;
+  }
+}
+
+export { SideForm, SideFormType };
