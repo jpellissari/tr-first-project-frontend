cb9129a1deac8b1f1df8f46992996fdfd4bdbd91
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-08 14:52:47 -0300

    refactor: edit employee component

--- src/app/employees/components/edit-employee/edit-employee.component.ts
+++ src/app/employees/components/edit-employee/edit-employee.component.ts
@@ -4,7 +4,6 @@ import {
   EventEmitter,
   Input,
   OnChanges,
-  OnDestroy,
   OnInit,
   Output,
   SimpleChanges
@@ -12,12 +11,14 @@ import {
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 import { Client } from 'src/app/clients/models/client';
 import { ClientsService } from 'src/app/clients/service/clients.service';
 import { JobPosition } from 'src/app/job-positions/models/job-position';
 import { JobPositionsService } from 'src/app/job-positions/service/job-positions.service';
+import { FormHelperService } from 'src/app/shared/services/form-helper.service';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 import { ToastService } from 'src/app/shared/services/toast.service';
 import { CpfCnpjValidator } from 'src/app/shared/validators/cpf-cnpj-validator';
@@ -29,18 +30,19 @@ import { EmployeesService } from '../../services/employees.service';
   templateUrl: './edit-employee.component.html',
   styleUrls: ['./edit-employee.component.scss']
 })
-export class EditEmployeeComponent implements OnInit, OnChanges, OnDestroy {
-  @Input() employee!: Employee;
-  @Output() formClosedEvent = new EventEmitter<void>();
-  @Output() employeeUpdatedEvent = new EventEmitter<void>();
+export class EditEmployeeComponent implements OnInit, OnChanges {
+  @Input() employee: Employee = {} as Employee;
+  @Output() formClosed = new EventEmitter<void>();
+  @Output() employeeUpdated = new EventEmitter<void>();
+
   submitted: boolean = false;
-  form!: FormGroup;
-  clients$!: Observable<Client[]>;
-  jobs$!: Observable<JobPosition[]>;
-  error$ = new Subject<boolean>();
-  clientOptions: Array<{ value: string; text: string }> = [];
+  form: FormGroup = {} as FormGroup;
+
+  jobs$: Observable<JobPosition[]> = of();
+  clients$: Observable<Client[]> = of();
 
   constructor(
+    public readonly formHelper: FormHelperService,
     private readonly formBuilder: FormBuilder,
     private readonly employeesService: EmployeesService,
     private readonly clientsService: ClientsService,
@@ -52,9 +54,9 @@ export class EditEmployeeComponent implements OnInit, OnChanges, OnDestroy {
   ) {}
 
   ngOnInit(): void {
+    this.form = this.createForm();
     this.clients$ = this.clientsService.list();
     this.jobs$ = this.jobPositionsService.list();
-    this.form = this.createForm();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -70,6 +72,72 @@ export class EditEmployeeComponent implements OnInit, OnChanges, OnDestroy {
         active: changes.employee.currentValue.active,
         type: changes.employee.currentValue.type.toString().toLowerCase()
       });
+    }
+  }
+
+  searchClient(value: string): void {
+    this.clients$ = this.clientsService
+      .list()
+      .pipe(
+        map((clients) =>
+          clients.filter(
+            (client) => client.name.toLocaleLowerCase().indexOf(value) > -1
+          )
+        )
+      );
+  }
+
+  searchJob(value: string): void {
+    this.jobs$ = this.jobPositionsService
+      .list()
+      .pipe(
+        map((jobs) =>
+          jobs.filter((job) => job.name.toLocaleLowerCase().indexOf(value) > -1)
+        )
+      );
+  }
+
+  deleteEmployee(): void {
+    this.employeesService
+      .delete(this.employee.id)
+      .pipe(take(1))
+      .subscribe(
+        (success) => {
+          const message = this.getTranslationMessageFor(
+            'employees.forms.delete.success'
+          );
+          this.handleSuccess(message);
+        },
+        (error) => {
+          this.handleError(error);
+        }
+      );
+  }
+
+  updateEmployee() {
+    if (this.form.valid) {
+      this.loading.start();
+
+      const employee = this.form.getRawValue();
+      employee.birthdate = this.datePipe.transform(
+        employee.birthdate,
+        'dd/MM/yyyy'
+      );
+
+      this.employeesService
+        .update(employee)
+        .pipe(take(1))
+        .subscribe(
+          (success) => {
+            const message = this.getTranslationMessageFor(
+              'employees.forms.edit.success'
+            );
+            this.handleSuccess(message);
+          },
+          (error) => {
+            this.handleError(error);
+          }
+        );
     }
   }
 
@@ -93,77 +161,25 @@ export class EditEmployeeComponent implements OnInit, OnChanges, OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    this.resetForm();
-  }
-
-  closeForm(): void {
-    this.resetForm();
-    this.formClosedEvent.emit();
-  }
-
-  hasError(field: string) {
-    return this.form.get(field)?.errors;
-  }
-
-  deleteEmployee(): void {
-    this.employeesService.delete(this.employee.id).subscribe(
-      (success) => {
-        this.translate
-          .get('employees.deleteEmployee.success')
-          .subscribe((message: string) => this.handleSuccess(message));
-      },
-      (error) => {
-        this.handleError(error);
-      }
-    );
-  }
-
-  updateEmployee() {
-    this.submitted = true;
-    this.loading.start();
-    if (this.form.valid) {
-      const employee = this.form.getRawValue();
-      employee.birthdate = this.datePipe.transform(
-        employee.birthdate,
-        'dd/MM/yyyy'
-      );
-      this.employeesService.update(employee).subscribe(
-        (success) => {
-          this.translate
-            .get('employees.editEmployee.success')
-            .subscribe((message: string) => this.handleSuccess(message));
-        },
-        (error) => {
-          this.handleError(error);
-        }
-      );
-    }
-    this.loading.stop();
-  }
-
-  private resetForm(): void {
-    this.loading.stop();
-    this.submitted = false;
-    this.form.reset();
+  private getTranslationMessageFor(key: string): string {
+    return this.translate.instant(key);
   }
 
   private handleSuccess(message: string): void {
-    this.resetForm();
+    this.loading.stop();
     this.toastService.showSuccessMessage(message);
-    this.employeeUpdatedEvent.emit();
-    this.formClosedEvent.emit();
+
+    this.employeeUpdated.emit();
+    this.formClosed.emit();
   }
 
   private handleError(error: any): void {
-    this.error$.next(true);
     this.loading.stop();
-
-    console.error('Error editing employee', error);
 
     if (error.status === 409) {
       this.translate
-        .get('employees.addEmployee.errors.conflit')
+        .get('employees.forms.errors.conflit')
+        .pipe(take(1))
         .subscribe((message: string) =>
           this.toastService.showErrorMessage(message)
         );
