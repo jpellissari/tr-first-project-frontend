1de1d3b1eb61cdcd82e5139b0707819242bc53fa
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-08 14:11:14 -0300

    feat: add mask to base input

--- src/app/shared/components/form/custom-form-components.module.ts
+++ src/app/shared/components/form/custom-form-components.module.ts
@@ -6,15 +6,28 @@ import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { DeleteButtonComponent } from './delete-button/delete-button.component';
 import { InputComponent } from './input/input.component';
 import { ValidationComponent } from './validation/validation.component';
+import { BaseInputComponent } from './base-input/base-input.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
-  declarations: [InputComponent, ValidationComponent, DeleteButtonComponent],
+  declarations: [
+    InputComponent,
+    ValidationComponent,
+    DeleteButtonComponent,
+    BaseInputComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     NzPopconfirmModule,
-    NzIconModule
+    NzIconModule,
+    NgxMaskModule
   ],
-  exports: [InputComponent, DeleteButtonComponent, ValidationComponent]
+  exports: [
+    InputComponent,
+    DeleteButtonComponent,
+    ValidationComponent,
+    BaseInputComponent
+  ]
 })
 export class CustomFormComponentsModule {}
